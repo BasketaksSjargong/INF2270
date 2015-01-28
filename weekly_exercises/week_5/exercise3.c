62c9a6cb9dc24154020fd1cf49eba011c400f5ad
BasketaksSjargong <tusslejargon@gmail.com>
2015-01-28 17:48:07 +0100

    Fixed an error that made the program not function properly

--- weekly_exercises/week_5/exercise3.c
+++ weekly_exercises/week_5/exercise3.c
@@ -5,14 +5,15 @@
 int main(void)
 {
   int t = (time(NULL)/3600 + 1) % 24; // Current hour  
-  if(6 <= t <= 9) {
+  if(6 <= t && t <= 9) {
     printf("God morgen, alle sammen!\n");
-  } else if(10 <= t <= 17) {
+  } else if(10 <= t && t <= 17) {
     printf("God dag, alle sammen!\n");
-  } else if(18 <= t <= 21) {
+  } else if(18 <= t && t <= 21) {
     printf("God kveld, alle sammen!\n");
   } else {
     printf("God natt, alle sammen!\n");
   }
+  printf("Klokken er %d\n", t);
   return 0;
 }
